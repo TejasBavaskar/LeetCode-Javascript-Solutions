8716a9c854d53ae7297a16a2c5f3904d22c22312
Tejas Bavaskar <tejkar777@gmail.com>
2021-06-10 09:59:25 +0530

    binary search format document

--- Binary Search/Easy/349.Intersection of Two Arrays/BruteForce.js
+++ Binary Search/Easy/349.Intersection of Two Arrays/BruteForce.js
@@ -1,19 +1,19 @@
 //O(m+n)
-var intersection = function(nums1, nums2) {
+var intersection = function (nums1, nums2) {
   let dict = {};
   let mySet = new Set();
-  
-  for(let i=0; i<nums1.length; i++) {
-      if(!(nums1[i] in dict)) {
-          dict[nums1[i]] = 1;
-      }
+
+  for (let i = 0; i < nums1.length; i++) {
+    if (!(nums1[i] in dict)) {
+      dict[nums1[i]] = 1;
+    }
   }
-  
-  for(let i=0; i<nums2.length; i++) {
-      if(nums2[i] in dict) {
-          mySet.add(nums2[i]);
-      }
+
+  for (let i = 0; i < nums2.length; i++) {
+    if (nums2[i] in dict) {
+      mySet.add(nums2[i]);
+    }
   }
-  
+
   return Array.from(mySet);
 };
